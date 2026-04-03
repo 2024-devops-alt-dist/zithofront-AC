4ad03ae83e9f8396c3f327d4873b281f137e17e1
Agnès Cappello <senga.ds@gmail.com>
2026-04-03 16:30:26 +0200

    alert info datas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom';
 import './../styles/HomeStyle.css';
 
 function Home() {
-  return (
+alert("La base de données est déployée avec Render, les données mettent du temps à charger (1-2 minutes) !");
 
+  return (
       <div className='home-container'>
         <h2 className='home-title'>The Beer Lover's Guide</h2>
         <Link to="/beers" className='link-style'>See all beers</Link>
